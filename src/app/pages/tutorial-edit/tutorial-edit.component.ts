e3293ec8f0ab4172d14207537d6d228957f9b387
Enamul haque <enamulcs008@gmail.com>
2020-10-26 07:46:05 +0530

    tutorial connected via Api

--- src/app/pages/tutorial-edit/tutorial-edit.component.ts
+++ src/app/pages/tutorial-edit/tutorial-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MainServicesService } from 'src/app/main-services.service';
 
 @Component({
   selector: 'app-tutorial-edit',
@@ -9,13 +11,44 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class TutorialEditComponent implements OnInit {
   TutorialForm: FormGroup;
   urls = [];
-  constructor() {
+  Id: any;
+  
+  constructor(public router: Router, public service: MainServicesService) {
     this.TutorialForm=new FormGroup({
       'image':new FormControl('')
     })
    }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getStaticId();
+    this.viewTutorial()
+  }
+
+  viewTutorial(){
+    let apireq = {
+      type: 'TUTORIAL'  
+      }
+      console.log('TutorialReq ==>>', apireq)
+      this.service.postApi('static/getStaticContent',apireq,1).subscribe((success:any)=> {
+       //console.log('Tutorial Views:',success)
+        this.urls=success.result.image;
+       
+       if (success.responseCode === 200) {
+          
+            localStorage.setItem('tutorial',JSON.stringify(success.result._id));
+       }
+    },error => {
+      console.log('Something went wrong');
+    })
+  }
+
+
+  
+  getStaticId(){
+    this.Id = JSON.parse(localStorage.getItem('tutorial'));
+    //console.log('id ===>>',this.Id);
+
+  }
 
   onSelectFile(event) {
     if (event.target.files && event.target.files[0]) {
@@ -23,15 +56,33 @@ export class TutorialEditComponent implements OnInit {
       for (let i = 0; i < filesAmount; i++) {
         var reader = new FileReader();
         reader.onload = (event: any) => {
-          console.log(event.target.result);
+      //    console.log(event.target.result);
           this.urls.push(event.target.result);
         }
         reader.readAsDataURL(event.target.files[i]);
-        console.log('urls==>', this.urls)
+        //console.log('urls==>', this.urls)
       }
     }
   }
 UpdateTutorial(){
-  
+  let apireq={
+    staticId:this.Id,
+    type: 'TUTORIAL',
+    image:this.urls
+  }
+  console.log('ReqObject:',apireq)
+  this.service.postApi(`static/updateStaticContent`,apireq,1).subscribe(response=>{
+    if(response["responseCode"]==200){
+      console.log("updated Data=====",response)
+      this.service.showSuccess('Tutorial updated successfully')
+    this.router.navigate(['static-content-management'])
+    }
+    else{
+      this.service.toastErr(response['responseMessage'])
+    }
+    },error=>{
+      this.service.toastErr('Something went wrong')
+    
+    })
 }
 }
--- src/app/pages/tutorials-view/tutorials-view.component.ts
+++ src/app/pages/tutorials-view/tutorials-view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MainServicesService } from 'src/app/main-services.service';
 
 @Component({
   selector: 'app-tutorials-view',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tutorials-view.component.css']
 })
 export class TutorialsViewComponent implements OnInit {
+  Images: any;
 
-  constructor() { }
+  constructor(public router: Router, public service: MainServicesService) { }
 
   ngOnInit() {
+    this.viewTutorial()
   }
 
+  viewTutorial(){
+    let apireq = {
+      type: 'TUTORIAL'  
+      }
+      console.log('TutorialReq ==>>', apireq)
+      this.service.postApi('static/getStaticContent',apireq,1).subscribe((success:any)=> {
+       //console.log('Tutorial Views:',success)
+        this.Images=success.result.image;
+        console.log("Image are:--->",this.Images)
+       if (success.responseCode === 200) {
+          
+            localStorage.setItem('tutorial',JSON.stringify(success.result._id));
+       }
+    },error => {
+      console.log('Something went wrong');
+    })
+  }
 }
